217492b68dfcdd620aae9637d5210c508daa0d49
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-01-14 20:04:22 -0500

    index page displaying

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 from .models import City
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
